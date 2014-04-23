435bbead606354d456fd95489caf5ba45b794fe1
Scott Mebberson <scott@scottmebberson.com>
2014-04-23 13:21:10 +0930

    Added the first version of the application.

---
+++ app/alternative.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+	linz = require('linz'),
+	port = 3700,
+	admin;
+
+// initialize linz, without express (it will be provided)
+admin = linz.init({
+	'admin path': '/webtop',
+	'admin title': 'MMS'
+});
+
+// start the app
+http.createServer(admin.app).listen(port, function(){
+	console.log('mini-twitter app started and running on port %s', port);
+});
