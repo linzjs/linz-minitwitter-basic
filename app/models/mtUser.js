a38bc7d32a705e23933ec0d3412c9f8b0606572b
Scott Mebberson <scott@scottmebberson.com>
2018-02-21 11:08:10 +1030

    Added the title property.

--- app/models/mtUser.js
+++ app/models/mtUser.js
@@ -1,93 +1,87 @@
 var linz = require('linz');
 
 var mtUserSchema = new linz.mongoose.Schema({
-	name:  String,
-	email: String,
-	username: String,
-	password: String,
-	bAdmin: {
-		type: Boolean,
-		default: false
-	}
+    name:  String,
+    email: String,
+    username: String,
+    password: String,
+    bAdmin: {
+        type: Boolean,
+        default: false
+    }
 });
 
 // add the formtools plugin
 mtUserSchema.plugin(linz.formtools.plugins.document, {
-	model: {
+    form: {
+        name: {
+            fieldset: 'Details',
+            helpText: 'The users full name.',
+        },
+        email: {
+            fieldset: 'Details',
+        },
+        username: {
+            fieldset: 'Access',
+        },
+        password: {
+            fieldset: 'Access',
+            widget: linz.formtools.widgets.password(),
+        },
+        bAdmin: {
+            fieldset: 'Access',
+            helpText: 'This controls if the user has access to admin.',
+        },
+    },
+    labels: {
+            name: 'Name',
+            email: 'Email',
+            bAdmin: 'Has admin access?',
+            username: 'Username',
+            password: 'Password'
+    },
+    list: {
+        fields: {
+            name: true,
+            username: true,
+            email: true,
+            bAdmin: true,
+        },
+    },
+    model: {
+        description: 'Manage users.',
         label: 'User',
-        description: 'Manage users.'
-  },
-	labels: {
-			name: 'Name',
-			email: 'Email',
-			bAdmin: 'Has admin access?',
-			username: 'Username',
-			password: 'Password'
-	},
-	permissions: function (user, callback) {
+        title: 'username',
+    },
+    overview: {
+        summary: {
+            fields: {
+                name: {
+                    renderer: linz.formtools.cellRenderers.defaultRenderer
+                },
+                email: {
+                    renderer: linz.formtools.cellRenderers.defaultRenderer
+                },
+            },
+        },
+    },
+    // Default empty object, which Linz can accept.
+    permissions: function (user, callback) {
 
-		// Default empty object, which Linz can accept.
-			// Defaults everything to true.
-			var perms = {
-					canCreate: false,
-					canDelete: false
-			};
+      return callback(null, {
+            canCreate: false,
+            canDelete: false
+        });
 
-      return callback(null, perms);
-
-  },
-	list: {
-		fields: {
-			name: true,
-			username: true,
-			email: true,
-			bAdmin: true
-		}
-	},
-	form: {
-		name: {
-			fieldset: 'Details',
-			helpText: 'The users full name.'
-		},
-		email: {
-			fieldset: 'Details'
-		},
-		username: {
-			fieldset: 'Access'
-		},
-		password: {
-			fieldset: 'Access',
-			widget: linz.formtools.widgets.password()
-		},
-		bAdmin: {
-			fieldset: 'Access',
-			helpText: 'This controls if the user has access to admin.'
-		}
-	},
-	overview: {
-		summary: {
-			fields: {
-				name: {
-					renderer: linz.formtools.cellRenderers.defaultRenderer
-				},
-				email: {
-					renderer: linz.formtools.cellRenderers.defaultRenderer
-				}
-			}
-		}
-	},
-	fields: {
-		usePublishingDate: false,
-		usePublishingStatus: false
-	}
+    },
 });
 
 mtUserSchema.virtual('hasAdminAccess').get(function () {
-	return this.bAdmin === true;
+    return this.bAdmin === true;
 });
 
 mtUserSchema.methods.verifyPassword = function (candidatePassword, callback) {
-	return callback(null, this.password === candidatePassword);
+    return callback(null, this.password === candidatePassword);
 }
 
 var mtUser = module.exports = linz.mongoose.model('mtUser', mtUserSchema);
